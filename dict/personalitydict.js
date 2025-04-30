ab4bde35d3e365a0c4cf73d7cc53ba3f64aa3a56
v e g a/spica M. <sakutattsu@yahoo.com>
2025-04-30 03:25:14 -0400

    umbrous > dusk

--- dict/personalitydict.js
+++ dict/personalitydict.js
@@ -201,6 +201,11 @@ const personalityJson = {
 		"back" : "aura_a_babyblue",
 		"front" : "a"
 	},
+	"Dusk": {
+		"each": "aura_s_blazing_tenebris",
+		"back": "back_b",
+		"front": "front_b"
+	},
 	"Elegant" : {
 		"back" : "aura_s_blackhole",
 		"front" : "aura_s_star"
@@ -526,11 +531,6 @@ const personalityJson = {
 	"Tired" : {
 		"back" : "aura_a_skywhite",
 		"front" : "a3_skywhite"
-	},
-	"Umbrous": {
-		"each": "aura_s_blazing_tenebris",
-		"back": "back_b",
-		"front": "front_b"
 	},
 	"Unknown" : {
 		"back" : "aura_s_universe3",
